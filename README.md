5baa75bd3bb89b463bbc8620519027f9ce4ec9e2
paulbutcher <paul.butcher@gmail.com>
2019-06-28 22:02:19 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+A Jupyter notebook for filling out the 
+[5conversations](https://isbnsearch.org/isbn/9781909623712) Future Focus Wheel 
+
+The Spider Plot code has been copied (almost) verbatim from the 
+[python graph gallery](https://python-graph-gallery.com/radar-chart/)
---
+++ spider.py
@@ -0,0 +1,78 @@
+# MIT License
+#
+# Copyright (c) 2017 Holtz Yan
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+# Libraries
+import matplotlib.pyplot as plt
+from math import pi
+
+# Set data
+
+
+def main(df):
+
+    # ------- PART 1: Create background
+
+    # number of variable
+    categories=list(df)[1:]
+    N = len(categories)
+
+    # What will be the angle of each axis in the plot? (we divide the plot / number of variable)
+    angles = [n / float(N) * 2 * pi for n in range(N)]
+    angles += angles[:1]
+
+    # Initialise the spider plot
+    ax = plt.subplot(111, polar=True)
+
+    # If you want the first axis to be on top:
+    ax.set_theta_offset(pi / 2)
+    ax.set_theta_direction(-1)
+
+    # Draw one axe per variable + add labels labels yet
+    plt.xticks(angles[:-1], categories)
+
+    # Draw ylabels
+    ax.set_rlabel_position(0)
+    plt.yticks([1,3,5,7,9], ["1","3","5","7","9"], color="grey", size=7)
+    plt.ylim(0,10)
+
+
+    # ------- PART 2: Add plots
+
+    # Plot each individual = each line of the data
+    # I don't do a loop, because plotting more than 3 groups makes the chart unreadable
+
+    # Ind1
+    values=df.loc[0].drop('group').values.flatten().tolist()
+    values += values[:1]
+    ax.plot(angles, values, linewidth=1, linestyle='solid', label="Now")
+    ax.fill(angles, values, 'b', alpha=0.1)
+
+    # Ind2
+    values=df.loc[1].drop('group').values.flatten().tolist()
+    values += values[:1]
+    ax.plot(angles, values, linewidth=1, linestyle='solid', label="Then")
+    ax.fill(angles, values, 'r', alpha=0.1)
+
+    # Add legend
+    plt.legend(loc='upper right', bbox_to_anchor=(0.1, 0.1))
+    plt.show()
